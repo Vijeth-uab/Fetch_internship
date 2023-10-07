4516e84053e0bc51e225e7af8d1b526c842b1cd5
vijeth-v <vijeth1410@gmail.com>
2023-10-07 01:49:30 -0500

    changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,72 @@
 <template>
-  <div class="container mx-auto px-5 py-2 lg:px-5 lg:pt-12">
-    <div class="flex justify-center mb-10">
+  <header
+    class="bg-gray-300"
+    style="
+      height: 70px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-bottom: 20px;
+    ">
+    <h1>Welcome to the gallery of dogs.</h1>
+  </header>
+  <div class="container mx-auto">
+    <div class="flex justify-center mb-5">
       <Listbox as="div" v-model="selected">
-        <ListboxLabel class="block text-sm font-medium leading-6 text-gray-900">Select Dog breed</ListboxLabel>
-        <div class="relative mt-2">
+        <ListboxLabel class="block text-sm font-medium leading-6 text-gray-900"
+          >Select Dog breed</ListboxLabel
+        >
+        <div class="relative">
           <ListboxButton
-            class="relative w-full cursor-default rounded-md bg-white py-1.5 pl-4 pr-40 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
+            class="relative w-100 cursor-default rounded-md bg-white py-1.5 pl-4 pr-40 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
             <span class="inline-flex w-full truncate">
-              <!-- <span class="truncate">{{ selected }}</span> -->
-              <span class="ml-2 truncate text-gray-500">{{ selected.name }}</span>
+              <span class="ml-2 truncate text-gray-500">{{
+                selected.breedName
+              }}</span>
             </span>
-            <span class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-              <ChevronUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+            <span
+              class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+              <ChevronUpDownIcon
+                class="h-5 w-5 text-gray-400"
+                aria-hidden="true" />
             </span>
           </ListboxButton>
 
-          <transition leave-active-class="transition ease-in duration-100" leave-from-class="opacity-100"
+          <transition
+            leave-active-class="transition ease-in duration-100"
+            leave-from-class="opacity-100"
             leave-to-class="opacity-0">
             <ListboxOptions
               class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-              <ListboxOption as="template" v-for="person in dogsList" :key="person.name" :value="person"
+              <ListboxOption
+                as="template"
+                v-for="person in dogsList"
+                :key="person.breedName"
+                :value="person"
                 v-slot="{ active, selected }">
                 <li
-                  :class="[active ? 'bg-indigo-600 text-white' : 'text-gray-900', 'relative cursor-default select-none py-2 pl-3 pr-9']"
+                  :class="[
+                    active ? 'bg-indigo-600 text-white' : 'text-gray-900',
+                    'relative cursor-default select-none py-2 pl-3 pr-9',
+                  ]"
                   @click="fetchDogs">
                   <div class="flex">
-                    <span :class="[selected ? 'font-semibold' : 'font-normal', 'truncate']">{{ person.name }}</span>
-                    <!-- <span :class="[active ? 'text-indigo-200' : 'text-gray-500', 'ml-2 truncate']">{{ person.username }}</span> -->
+                    <span
+                      :class="[
+                        selected ? 'font-semibold' : 'font-normal',
+                        'truncate',
+                      ]"
+                      >{{ person.breedName }}</span
+                    >
+                    <!-- <span :class="[active ? 'text-indigo-200' : 'text-gray-500', 'ml-2 truncate']">{{ person.dogBreedName }}</span> -->
                   </div>
 
-                  <span v-if="selected"
-                    :class="[active ? 'text-white' : 'text-indigo-600', 'absolute inset-y-0 right-0 flex items-center pr-4']">
+                  <span
+                    v-if="selected"
+                    :class="[
+                      active ? 'text-white' : 'text-indigo-600',
+                      'absolute inset-y-0 right-0 flex items-center pr-4',
+                    ]">
                     <CheckIcon class="h-5 w-5" aria-hidden="true" />
                   </span>
                 </li>
@@ -42,13 +78,21 @@
     </div>
 
     <div class="flex justify-center mb-10 capitalize">
-      <h1>following images are from <span class="font-mono text-xl">{{ selected.name }}</span></h1>
+      <h1>
+        <b>{{ selected.breedName }}</b> breed's images are displayed
+      </h1>
     </div>
-    <div class="-m-1 flex flex-wrap  mt-10 md:-m-2">
-      <div class="flex w-1/4 flex-wrap" v-for="(imageUrl, index) in imageUrlsArray" :key="index">
+    <div class="-m-1 flex flex-wrap md:-m-2">
+      <div
+        class="flex w-1/4 flex-wrap"
+        v-for="(imageUrl, index) in imageUrlsArray"
+        :key="index">
         <div class="w-full p-1 md:p-2">
-          <img alt="gallery" class="block h-full w-full rounded-lg object-cover object-center" :src="imageUrl"
-            max-width="200" max-height="200" />
+          <img
+            alt="gallery"
+            class="block h-full w-full rounded-lg object-cover object-center"
+            :src="imageUrl"
+            style="max-width: 300px; max-height: 300px" />
         </div>
       </div>
     </div>
@@ -57,9 +101,14 @@
 
 <script setup>
 import { onMounted, ref } from "vue";
-import { Listbox, ListboxButton, ListboxLabel, ListboxOption, ListboxOptions } from '@headlessui/vue'
-import { CheckIcon, ChevronUpDownIcon } from '@heroicons/vue/20/solid'
-
+import {
+  Listbox,
+  ListboxButton,
+  ListboxLabel,
+  ListboxOption,
+  ListboxOptions,
+} from "@headlessui/vue";
+import { CheckIcon, ChevronUpDownIcon } from "@heroicons/vue/20/solid";
 
 onMounted(() => {
   fetchDogsList();
@@ -70,40 +119,40 @@ const imageUrls = ref("");
 const imageUrlsArray = ref([]);
 const dogsList = ref([]);
 
-const selected = ref({ name: 'airedale', username: '@affenpinscher' })
+const selected = ref({ breedName: "airedale", dogBreedName: "@affenpinscher" });
 
 async function fetchDogsList() {
   const response = await fetch("https://dog.ceo/api/breeds/list/all");
   const data = await response.json();
   const dataObject = Object.keys(data.message);
-  dogsList.value = Object.keys(data.message).map(breed => {
-    const name = breed; // Capitalize the breed name
-    const username = `@${breed}`; // Create a username based on the breed name
-    return { name, username };
+  dogsList.value = Object.keys(data.message).map((breed) => {
+    const breedName = breed;
+    const dogBreedName = `@${breed}`;
+    return { breedName, dogBreedName };
   });
 
-  console.log(dogsList.value[0])
-
+  console.log(dogsList.value[0]);
 }
 
 async function fetchDogs() {
-  await fetch(`https://dog.ceo/api/breed/${selected.value.name}/images/random/20`)
-    .then(response => {
+  await fetch(
+    `https://dog.ceo/api/breed/${selected.value.breedName}/images/random/20`
+  )
+    .then((response) => {
       if (response.status === 301) {
-        const newURL = response.headers.get('Location');
-        // Make future requests to newURL if necessary
+        const newURL = response.headers.get("Location");
         return fetch(newURL);
       } else {
         return response.json(); // Convert the response to JSON
       }
     })
-    .then(jsonData => {
-      console.log("jsonData.message", jsonData.message)
+    .then((jsonData) => {
+      console.log("jsonData.message", jsonData.message);
       imageUrls.value = jsonData.message;
       imageUrlsArray.value = jsonData.message;
     })
-    .catch(error => {
-      console.error('Error:', error);
+    .catch((error) => {
+      console.error("Error:", error);
     });
 }
 </script>
